59d055d8c34271a003fe5550670e7fddfa7d1c80
Akash <akashs1132000@gmail.com>
2025-02-03 16:54:50 +0530

    creating posts

---
+++ server/models/postMessage.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+//schema
+const postSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    },
+});
+
+// turn it out to model
+
+const PostMessage = mongoose.model('postMessage', postSchema);
+
+export default PostMessage;
